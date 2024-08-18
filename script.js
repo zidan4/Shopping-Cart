d7eb3e82252dc14ef36ba341205c207ab8612de2
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-18 23:01:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cartContainer = document.getElementById("cart-container");
+
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
 const cartBtn = document.getElementById("cart-btn");
